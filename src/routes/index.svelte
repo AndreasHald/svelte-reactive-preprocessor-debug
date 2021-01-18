1cd6fe6d490c65bfd54be886693fb410a606cc43
Andreas Hald <andreas.boris.hald@gmail.com>
2021-01-18 15:36:30 +0100

    Added sapper starter and reactivePreproccesor

---
+++ src/routes/index.svelte
@@ -0,0 +1,62 @@
+<script>
+  import successkid from "images/successkid.jpg";
+
+  let num = 1;
+  $: double = num * 2;
+</script>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<figure>
+  <img alt="Success Kid" src={successkid} />
+  <figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<button on:click={() => (num = num + 1)}>add num</button>
+<p>{num}</p>
+<p>{double}</p>
+<p>
+  <strong
+    >Try editing this file (src/routes/index.svelte) to test live reloading.</strong
+  >
+</p>
+
+<style>
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
